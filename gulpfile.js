a18ac335634ad7d0ea4858909deaec6bc9cc8f4a
Olivier Louvignes <olivier@mg-crea.com>
2014-10-24 13:48:54 +0200

    chore(gulp): initial update

--- gulpfile.js
+++ gulpfile.js
@@ -7,29 +7,22 @@ var pkg = require('./package.json');
 //
 var config = require('./ngfactory.json');
 config.pkg = pkg;
-require('ng-factory').app(gulp, config);
-// gulp, pkg, module, type(app/comp), {+++}.
-
+require('ng-factory')(gulp, config);
 
 // TASKS
 //
-var src = config.src;
-gulp.task('dist/copy', function() {
-  // gulp.src(['bower_components/jsoneditor/jsoneditor.min.css', 'bower_components/jsoneditor/img/*.png'], {cwd: src.cwd, base: src.cwd})
-  //   .pipe(gulp.dest(src.dest));
-});
-
-// ALIASES
-//
 var runSequence = require('run-sequence');
-gulp.task('default', ['build']);
-gulp.task('dist', ['build']);
-gulp.task('test', function() {
-  runSequence('ng-factory:test/clean', 'ng-factory:src/karma~init', 'ng-factory:src/karma');
-});
 gulp.task('build', function() {
-  runSequence('ng-factory:dist/clean', 'ng-factory:src/views', ['ng-factory:dist/views', 'ng-factory:dist/copy'], 'dist/copy');
+  runSequence('ng-factory:dist/clean', ['ng-factory:dist/templates', 'ng-factory:dist/scripts', 'ng-factory:dist/styles']);
 });
 gulp.task('serve', function() {
   runSequence('ng-factory:src/clean', 'ng-factory:src/views', ['ng-factory:src/serve', 'ng-factory:src/watch']);
 });
+gulp.task('test', function() {
+  runSequence('ng-factory:test/clean', 'ng-factory:src/karma~init', 'ng-factory:src/karma');
+});
+
+// ALIASES
+//
+gulp.task('default', ['build']);
+gulp.task('dist', ['build']);
