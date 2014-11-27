e6e0223ec2125b83e053baceea9fce3c7c204879
Olivier Louvignes <olivier@mg-crea.com>
2014-11-27 20:02:53 +0100

    refactor(gulp): switch to latest ngFactory

--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,22 @@
 'use strict';
 
-var gulp = require('gulp');
-var run = require('run-sequence');
-
-// CONFIG
 //
+// Required modules
+
+var gulp = require('gulp');
+
+//
+// ngFactory setup
+
 var config = require('./ngfactory.json');
-var pkg = require('./package.json');
-config.pkg = pkg;
-var bower = require('./bower.json');
-config.bower = bower;
-
-config.requireTransform = function(name){
-  return require('./transforms/' + name);
-};
-
-
+config.pkg = require('./package.json');
 require('ng-factory')(gulp, config);
 
-
-// TASKS
 //
-var runSequence = require('run-sequence');
-gulp.task('build', function() {
-  runSequence('ng:dist/clean', ['ng:dist/templates', 'ng:dist/scripts', 'ng:dist/styles']);
-});
-gulp.task('test', function() {
-  runSequence('ng:test/clean', ['ng:test/templates', 'ng:test/karma~init'], ['ng:test/karma', 'ng:test/jshint']);
-});
-gulp.task('serve', function() {
-  runSequence('ng:docs/clean', 'ng:docs/views', ['ng:docs/serve', 'ng:docs/watch']);
-});
-gulp.task('readme', function (cb) {
-  runSequence('ng-factory:docs/ngdocs', 'ng-factory:docs/readme', cb);
-});
+// Example of custom tasks using before/after hooks
 
-gulp.task('release', function(cb){
-  run(
-    'ng-factory:src/bump',
-    'ng-factory:src/deploy:configPkgUpdate',
-    'ng-factory:src/changelog',
-    'ng-factory:src/deploy:src',
-    cb);
-});
-
-
-// ALIASES
-//
-gulp.task('default', ['build']);
-gulp.task('dist', ['build']);
+// var src = config.src;
+// gulp.task('ng:afterBuild', function() {
+//   gulp.src(['...'], {cwd: src.cwd, base: src.cwd})
+//     .pipe(gulp.dest(src.dest));
+// });
